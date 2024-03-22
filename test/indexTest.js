c5fbed73b91baa65c114e959e50bb3fe7df98340
Craig <craig.mwangi@student.moringaschool.com>
2024-03-22 14:42:29 +0300

    Labs

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,31 @@
 require ( './helpers.js' );
+function scuberGreetingForFeet(distance){
+  if(distance <= 400){
+    return "This one is on me!";
+  } else if(distance > 400 && distance <= 2000){
+    return "That will be twenty bucks.";
+  } else if(distance > 2000 && distance <= 2500){
+    return "I will gladly take your thirty bucks.";
+  } else if(distance > 2500){
+    return "No can do.";
+  }
+}
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous'){
+    return "Thank you so much.";
+  } else if (tip === 'not as generous'){
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
+}
 
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+}
 describe('index.js', function () {
   describe('scuberGreetingForFeet()', function () {
     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
